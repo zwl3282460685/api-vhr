9d08ff67e33fbc0ff6e3f4ecbb6c285e8883b392
zwl3242460685 <1312480826@qq.com>
2020-12-05 22:25:27 +0800

    解决在未登录时，重定向所造成的跨域问题

--- src/main/java/com/zwl/vhrapi/config/SecurityConfig.java
+++ src/main/java/com/zwl/vhrapi/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.zwl.vhrapi.model.Hr;
 import com.zwl.vhrapi.model.RespBean;
 import com.zwl.vhrapi.service.HrService;
+import io.swagger.models.auth.In;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.*;
@@ -16,6 +17,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
@@ -67,71 +69,83 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
 //                .anyRequest().authenticated()
-                .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
-                    @Override
-                    public <O extends FilterSecurityInterceptor> O postProcess(O o) {
-                        o.setAccessDecisionManager(myDecisionManager);
-                        o.setSecurityMetadataSource(myFilter);
-                        return o;
+            .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
+                @Override
+                public <O extends FilterSecurityInterceptor> O postProcess(O o) {
+                    o.setAccessDecisionManager(myDecisionManager);
+                    o.setSecurityMetadataSource(myFilter);
+                    return o;
+                }
+            })
+            .and()
+            .formLogin()
+            .usernameParameter("username")
+            .passwordParameter("password")
+            .loginProcessingUrl("/doLogin")
+            .loginPage("/login")
+            .successHandler(new AuthenticationSuccessHandler() {
+                @Override
+                public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+                                                    Authentication authentication) throws IOException, ServletException {
+                    response.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = response.getWriter();
+                    Hr hr = (Hr) authentication.getPrincipal(); //登录成功的hr对象
+                    hr.setPassword(null);
+                    RespBean ok = RespBean.ok("登录成功！", hr);
+                    out.write(new ObjectMapper().writeValueAsString(ok));
+                    out.flush();
+                    out.close();
+                }
+            })
+            .failureHandler(new AuthenticationFailureHandler() {
+                @Override
+                public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse rep, AuthenticationException e) throws IOException, ServletException {
+                    rep.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = rep.getWriter();
+                    RespBean error = RespBean.error("登录失败!");
+                    if(e instanceof LockedException){
+                        error.setMsg("账户被锁定，请联系管理员");
+                    }else if(e instanceof CredentialsExpiredException){
+                        error.setMsg("密码过期，请联系管理员");
+                    }else if(e instanceof AccountExpiredException){
+                        error.setMsg("账户过期，请联系管理员");
+                    }else if(e instanceof DisabledException){
+                        error.setMsg("账户禁用，请联系管理员");
+                    }else if(e instanceof BadCredentialsException){
+                        error.setMsg("用户名或密码输入错误，请重新输入");
                     }
-                })
-                .and()
-                .formLogin()
-                .usernameParameter("username")
-                .passwordParameter("password")
-                .loginProcessingUrl("/doLogin")
-                .loginPage("/login")
-                .successHandler(new AuthenticationSuccessHandler() {
-                    @Override
-                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
-                                                        Authentication authentication) throws IOException, ServletException {
-                        response.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = response.getWriter();
-                        Hr hr = (Hr) authentication.getPrincipal(); //登录成功的hr对象
-                        hr.setPassword(null);
-                        RespBean ok = RespBean.ok("登录成功！", hr);
-                        out.write(new ObjectMapper().writeValueAsString(ok));
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .failureHandler(new AuthenticationFailureHandler() {
-                    @Override
-                    public void onAuthenticationFailure(HttpServletRequest req, HttpServletResponse rep, AuthenticationException e) throws IOException, ServletException {
-                        rep.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = rep.getWriter();
-                        RespBean error = RespBean.error("登录失败!");
-                        if(e instanceof LockedException){
-                            error.setMsg("账户被锁定，请联系管理员");
-                        }else if(e instanceof CredentialsExpiredException){
-                            error.setMsg("密码过期，请联系管理员");
-                        }else if(e instanceof AccountExpiredException){
-                            error.setMsg("账户过期，请联系管理员");
-                        }else if(e instanceof DisabledException){
-                            error.setMsg("账户禁用，请联系管理员");
-                        }else if(e instanceof BadCredentialsException){
-                            error.setMsg("用户名或密码输入错误，请重新输入");
-                        }
-                        out.write(new ObjectMapper().writeValueAsString(error));
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .permitAll()
-                .and()
-                .logout()
-                .logoutSuccessHandler(new LogoutSuccessHandler() {
-                    @Override
-                    public void onLogoutSuccess(HttpServletRequest req, HttpServletResponse rep, Authentication authentication) throws IOException, ServletException {
-                        rep.setContentType("application/json;charset=utf-8");
-                        PrintWriter out = rep.getWriter();
-                        out.write(new ObjectMapper().writeValueAsString(RespBean.ok("注销成功！")));
-                        out.flush();
-                        out.close();
-                    }
-                })
-                .permitAll()
-                .and()
-                .csrf().disable();
+                    out.write(new ObjectMapper().writeValueAsString(error));
+                    out.flush();
+                    out.close();
+                }
+            })
+            .permitAll()
+            .and()
+            .logout()
+            .logoutSuccessHandler(new LogoutSuccessHandler() {
+                @Override
+                public void onLogoutSuccess(HttpServletRequest req, HttpServletResponse rep, Authentication authentication) throws IOException, ServletException {
+                    rep.setContentType("application/json;charset=utf-8");
+                    PrintWriter out = rep.getWriter();
+                    out.write(new ObjectMapper().writeValueAsString(RespBean.ok("注销成功！")));
+                    out.flush();
+                    out.close();
+                }
+            })
+            .permitAll()
+            .and()
+            .csrf().disable().exceptionHandling()
+            //没有认证时，在这里处理结果，不要重定向
+            .authenticationEntryPoint((request, rep, e) -> {
+                rep.setContentType("application/json;charset=utf-8");
+                PrintWriter out = rep.getWriter();
+                RespBean error = RespBean.error("访问失败!");
+                if(e instanceof InsufficientAuthenticationException){
+                    error.setMsg("请求失败，请联系管理员！");
+                }
+                out.write(new ObjectMapper().writeValueAsString(error));
+                out.flush();
+                out.close();
+            });
     }
 }

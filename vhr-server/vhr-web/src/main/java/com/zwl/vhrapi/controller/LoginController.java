42e3c638be0be055e21596ab318967f044688a73
张文亮 <1312480826@qq.com>
2021-02-16 21:13:08 +0800

    添加前端build的页面

--- vhr-server/vhr-web/src/main/java/com/zwl/vhrapi/controller/LoginController.java
+++ vhr-server/vhr-web/src/main/java/com/zwl/vhrapi/controller/LoginController.java
@@ -4,8 +4,10 @@ import com.zwl.vhrapi.config.VerificationCode;
 import com.zwl.vhrapi.model.RespBean;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletResponse;
@@ -26,6 +28,7 @@ public class LoginController {
 
     @GetMapping("/verifyCode")
     @ApiOperation("验证码生成接口")
+    @ResponseBody
     public void verifyCode(HttpSession httpSession, HttpServletResponse resp) throws IOException {
         VerificationCode code = new VerificationCode();
         BufferedImage image = code.getImage();
